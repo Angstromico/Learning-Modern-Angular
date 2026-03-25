3723c3cc2c1c258eadc064f64022a169357e8f28
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-25 15:43:33 -0400

    refactor: change detection in counter app

--- src/app/pages/counter/counter.component.ts
+++ src/app/pages/counter/counter.component.ts
@@ -1,12 +1,20 @@
-import { Component, signal } from "@angular/core";
+import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter.component.html',
-  styleUrl: './counter.component.css'
+  styleUrl: './counter.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CounterComponent {
   counter = 0
   counterSignals = signal(0)
+
+  constructor() {
+    setInterval(() => {
+      this.counterSignals.update(value => value + 1)
+    }, 1000)
+  }
+
   changeNumber = (change: '+' | '-') => {
     if(change === '+') {
       this.counter++
