77671c2a48707b2e80a835eac70e8c6673b11ae5
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-03-25 15:27:20 -0400

    feat: signals comparations

--- src/app/pages/counter/counter.component.ts
+++ src/app/pages/counter/counter.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, signal } from "@angular/core";
 
 @Component({
   templateUrl: './counter.component.html',
@@ -6,13 +6,18 @@ import { Component } from "@angular/core";
 })
 export class CounterComponent {
   counter = 0
+  counterSignals = signal(0)
   changeNumber = (change: '+' | '-') => {
     if(change === '+') {
       this.counter++
+      this.counterSignals.update(value => value + 1)
     }
     if(change === '-') {
       this.counter--
+      this.counterSignals.update(value => value - 1)
     }
   }
-  resetCounter = () => this.counter = 0
+  resetCounter = () => {this.counter = 0
+    this.counterSignals.set(0)
+  }
 }
