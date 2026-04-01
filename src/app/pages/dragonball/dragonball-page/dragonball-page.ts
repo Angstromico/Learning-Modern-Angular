412dd4ba2dcc9638e2315eeb35bac1f0561274b8
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-01 13:05:42 -0400

    feat: no selector in dragon ball page

--- src/app/pages/dragonball/dragonball-page/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page/dragonball-page.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-dragonball-page',
   imports: [],
   templateUrl: './dragonball-page.html',
   styleUrl: './dragonball-page.css',
