86998f601abbd2837bbe0b6d498a0038c9e87562
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-01 11:17:10 -0400

    feat: navbar component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,15 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/shared/navbar/navbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
-  styleUrl: './app.css'
-
+  styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('Learning Angular bases');
-  protected readonly author = signal('Manuel Morales')
+  protected readonly author = signal('Manuel Morales');
   protected readonly currentYear = signal(new Date().getFullYear());
 }
