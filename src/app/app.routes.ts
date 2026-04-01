7adcc8bff1e52708426f7b17edde9a102fbba388
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-01 11:52:23 -0400

    feat: dragon ball page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import { CounterComponent } from './pages/counter/counter.component';
 import { HeroComponent } from './pages/hero/hero.component';
+import { DragonballPage } from './pages/dragonball/dragonball-page/dragonball-page';
 
 export const routes: Routes = [
   {
@@ -11,6 +12,7 @@ export const routes: Routes = [
     path: 'hero',
     component: HeroComponent,
   },
+  { path: 'dragonball', component: DragonballPage },
   {
     path: '**',
     redirectTo: '',
