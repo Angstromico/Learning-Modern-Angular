3d774100dccd5aa82751e0eaeb62ab6e990c0b83
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-01 13:06:39 -0400

    feat: no selector in Hero component

--- src/app/pages/hero/hero.component.ts
+++ src/app/pages/hero/hero.component.ts
@@ -2,28 +2,26 @@ import { UpperCasePipe } from '@angular/common';
 import { ChangeDetectionStrategy, Component, OnInit, signal, computed } from '@angular/core';
 
 @Component({
-  selector: 'app-hero',
   standalone: true,
   templateUrl: './hero.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [UpperCasePipe]
+  imports: [UpperCasePipe],
 })
-
 export class HeroComponent implements OnInit {
-  ngOnInit() { }
+  ngOnInit() {}
 
   readonly name = signal('Manuel Morales');
   readonly age = signal(35);
-  readonly getHeroDescription = computed(() =>
-    `${this.name()} has the age of ${this.age()}`
-  );
-  readonly textData = computed(() =>
-    [this.name(), this.age().toString(), this.getHeroDescription()]
-  )
-  changeHero = () => this.name.update((name) => name = 'Esteban Zuarez')
-  changeAge = () => this.age.update((a) => a = 57)
+  readonly getHeroDescription = computed(() => `${this.name()} has the age of ${this.age()}`);
+  readonly textData = computed(() => [
+    this.name(),
+    this.age().toString(),
+    this.getHeroDescription(),
+  ]);
+  changeHero = () => this.name.update((name) => (name = 'Esteban Zuarez'));
+  changeAge = () => this.age.update((a) => (a = 57));
   resetForm = () => {
-    this.name.set('Manuel Morales')
-    this.age.set(35)
-  }
+    this.name.set('Manuel Morales');
+    this.age.set(35);
+  };
 }
