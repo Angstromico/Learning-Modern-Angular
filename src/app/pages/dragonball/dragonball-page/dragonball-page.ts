bd7b02e01e2cec44c0e2db3feb4008870aa8d6a2
Manuel Esteban Morales Zuarez <manuesteban1990@gmail.com>
2026-04-01 13:51:30 -0400

    feat: Dragon Ball form

--- src/app/pages/dragonball/dragonball-page/dragonball-page.ts
+++ src/app/pages/dragonball/dragonball-page/dragonball-page.ts
@@ -1,8 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, signal, type WritableSignal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './dragonball-page.html',
   styleUrl: './dragonball-page.css',
 })
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+  name = signal('');
+  ki = signal(0);
+
+  changeName = (name: string) => this.name.set(name);
+  changeKi = (ki: number) => this.ki.set(ki);
+
+  characters: WritableSignal<{ name: string; ki: number }[]> = signal([]);
+
+  addCharacter() {
+    if (!this.name().trim() || this.ki() === null || this.ki()! <= 0) {
+      return;
+    }
+
+    const newCharacter = {
+      name: this.name().trim(),
+      ki: this.ki()!,
+    };
+
+    this.characters.update((prev) => [...prev, newCharacter]);
+
+    this.name.set('');
+    this.ki.set(0);
+  }
+}
